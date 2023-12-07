c8a15c46838e072cacc2a814e90f122d9bb0ec57
chuansate <lowsimchuan@gmail.com>
2023-12-07 21:59:08 +0800

    Added Resume Button
    The Resume and Pause buttons are working as expected

--- main.py
+++ main.py
@@ -15,13 +15,21 @@ def create_pause_button(img, side_length, coord_top_left):
     cv2.rectangle(img, start_pt_white_bar2,
                   (start_pt_white_bar2[0] + white_bars_width, start_pt_white_bar2[1] + white_bars_height),
                   (255, 255, 255), -1)
-    cv2.imshow('inside create_pause_button', img)
 
+
+def create_resume_button(img, side_length, coord_top_left):
+    cv2.rectangle(img, coord_top_left, (coord_top_left[0] + side_length, coord_top_left[1] + side_length), (0, 255, 0), -1)
+    cv2.line(img, (coord_top_left[0] + side_length//3, coord_top_left[1] + side_length//3), (coord_top_left[0] + side_length//3, coord_top_left[1] + int(2/3*side_length)), (255, 255, 255), 2)
+    cv2.line(img, (coord_top_left[0] + side_length//3, coord_top_left[1] + int(2/3*side_length)),
+             (coord_top_left[0] + int(2/3*side_length), coord_top_left[1] + side_length//2), (255, 255, 255), 2)
+    cv2.line(img, (coord_top_left[0] + int(2/3*side_length), coord_top_left[1] + side_length//2),
+             (coord_top_left[0] + side_length//3, coord_top_left[1] + side_length//3), (255, 255, 255), 2)
 
 cap = cv2.VideoCapture(0)
 WIDTH = 300
 HEIGHT = 300
 PAUSE_BUTTON_SIDE_LENGTH = 45
+RESUME_BUTTON_SIDE_LENGTH = 45
 cv2.namedWindow("Frame", cv2.WINDOW_NORMAL)
 cv2.setWindowProperty("Frame", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
@@ -55,28 +63,32 @@ while True:
 
     # Draw a button using rectangle, if possible make the window to take up entire screen.
     coord_top_left_pause_button = (frame_width - 55, 10)
-
-    create_pause_button(frame, PAUSE_BUTTON_SIDE_LENGTH, coord_top_left_pause_button)
-
+    coord_top_left_resume_button = ((frame_width-RESUME_BUTTON_SIDE_LENGTH)//2, (frame_height-RESUME_BUTTON_SIDE_LENGTH)//2)
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
             # don't pass HAND_CONNECTIONS if u just want the landmarks
             mpDraw.draw_landmarks(frame, handLms)
             index_finger_tip_x = handLms.landmark[mpHands.HandLandmark.INDEX_FINGER_TIP].x * frame_width
             index_finger_tip_y = handLms.landmark[mpHands.HandLandmark.INDEX_FINGER_TIP].y * frame_height
-            if (coord_top_left_pause_button[0] <= index_finger_tip_x <=
+            # if the pause button is clicked by the index fingertip
+            if not game_paused and (coord_top_left_pause_button[0] <= index_finger_tip_x <=
                 coord_top_left_pause_button[0] + PAUSE_BUTTON_SIDE_LENGTH) and (
                     coord_top_left_pause_button[1] <= index_finger_tip_y <=
                     coord_top_left_pause_button[1] + PAUSE_BUTTON_SIDE_LENGTH):
                 game_paused = not game_paused
+            # if the resume button is clicked by the index fingertip
+            if game_paused and (coord_top_left_resume_button[0] <= index_finger_tip_x <=
+                coord_top_left_resume_button[0] + RESUME_BUTTON_SIDE_LENGTH) and (
+                    coord_top_left_resume_button[1] <= index_finger_tip_y <=
+                    coord_top_left_resume_button[1] + RESUME_BUTTON_SIDE_LENGTH):
+                game_paused = not game_paused
 
     # after clicking the pause button, spawn a green resume button at the middle
     # if the green button is still at the same place as the pause button, then the game will keep pausing and resuming
-
     if game_paused:
-        cv2.putText(frame, "PAUSED", (100, 100), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+        create_resume_button(frame, RESUME_BUTTON_SIDE_LENGTH, coord_top_left_resume_button)
     else:
-        cv2.putText(frame, "RESUMED", (100, 100), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+        create_pause_button(frame, PAUSE_BUTTON_SIDE_LENGTH, coord_top_left_pause_button)
     cv2.imshow('Frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
