c52e5ea3ba77a5cece8d1e98cee185bb3981bf26
chuansate <lowsimchuan@gmail.com>
2023-12-18 22:49:48 +0800

    Implemented a ball flying towards player
    The ball was created by loading an PNG image, the image might contain some unwanted parts, hence try using cv2.circle() instead.

--- test123.py
+++ test123.py
@@ -1,1 +1,12 @@
-print((2, 3) + (3, 3))
+import cv2
+import numpy as np
+
+# Create a black image of size 350x600
+image = np.zeros((600, 350, 3), dtype="uint8")
+
+# modifying pixels from 0 to 100 vertically, then from 200:300 horizontally
+image[0:100, 200:300] = (255, 255, 255)
+# Display the image
+cv2.imshow('Black Image', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
