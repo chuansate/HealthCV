7226f91045d45f36c9426d7af7fc6fd6ac8e0c83
chuansate <lowsimchuan@gmail.com>
2024-04-11 22:38:46 +0800

    New API for hand landmarks detection
    This is becoz mediapipe has modified the API, no longer using the old API!

--- NEW_API_FOR_HAND_LANDMARKS_DETECTION.py
+++ NEW_API_FOR_HAND_LANDMARKS_DETECTION.py
@@ -1,12 +1,12 @@
-import cv2
 import mediapipe as mp
+import cv2
 from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
-
 from mediapipe import solutions
 from mediapipe.framework.formats import landmark_pb2
 import numpy as np
+from mediapipe.tasks.python import vision
 
+MODEL_PATH = 'hand_landmarker.task'
 MARGIN = 10  # pixels
 FONT_SIZE = 1
 FONT_THICKNESS = 1
@@ -17,60 +17,71 @@ def draw_landmarks_on_image(rgb_image, detection_result):
     hand_landmarks_list = detection_result.hand_landmarks
     handedness_list = detection_result.handedness
     annotated_image = np.copy(rgb_image)
-
     # Loop through the detected hands to visualize.
     for idx in range(len(hand_landmarks_list)):
         hand_landmarks = hand_landmarks_list[idx]
         handedness = handedness_list[idx]
-
         # Draw the hand landmarks.
         hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
         hand_landmarks_proto.landmark.extend([
-            landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
+          landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
         ])
         solutions.drawing_utils.draw_landmarks(
-            annotated_image,
-            hand_landmarks_proto,
-            solutions.hands.HAND_CONNECTIONS,
-            solutions.drawing_styles.get_default_hand_landmarks_style(),
-            solutions.drawing_styles.get_default_hand_connections_style()
-        )
-
+          annotated_image,
+          hand_landmarks_proto,
+          solutions.hands.HAND_CONNECTIONS,
+          solutions.drawing_styles.get_default_hand_landmarks_style(),
+          solutions.drawing_styles.get_default_hand_connections_style())
         # Get the top left corner of the detected hand's bounding box.
         height, width, _ = annotated_image.shape
         x_coordinates = [landmark.x for landmark in hand_landmarks]
         y_coordinates = [landmark.y for landmark in hand_landmarks]
         text_x = int(min(x_coordinates) * width)
         text_y = int(min(y_coordinates) * height) - MARGIN
-
         # Draw handedness (left or right hand) on the image.
         cv2.putText(annotated_image, f"{handedness[0].category_name}",
                     (text_x, text_y), cv2.FONT_HERSHEY_DUPLEX,
                     FONT_SIZE, HANDEDNESS_TEXT_COLOR, FONT_THICKNESS, cv2.LINE_AA)
-
     return annotated_image
 
-# create an HandLandmarker object
-base_options = python.BaseOptions(model_asset_path='hand_landmarker.task')
+
+# Setup options
+base_options = python.BaseOptions(model_asset_path=MODEL_PATH)
 options = vision.HandLandmarkerOptions(base_options=base_options,
                                        num_hands=2)
 detector = vision.HandLandmarker.create_from_options(options)
 
-# define a video capture object
-vid = cv2.VideoCapture(0)
+
+# Get inference
+def get_annotation_from(frame):
+    image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+    detection_result = detector.detect(image)
+    annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
+
+    return detection_result, annotated_image
+
+
+cap = cv2.VideoCapture(0)
 
 while True:
-    ret, frame = vid.read()
-    rgb_frame = mp.Image(image_format=mp.ImageFormat.SRGB)
-    detection_result = detector.detect(rgb_frame)
-    annotated_image = draw_landmarks_on_image(frame.numpy_view(), detection_result)
-    # cv2_imshow(cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
-    cv2.imshow('frame', annotated_image)
+    # capture image
+    ret, frame = cap.read()
+
+    if ret:
+        frame = cv2.flip(frame, 1)
+        frame_height = frame.shape[0]
+        frame_width = frame.shape[1]
+        detection_result, annotation = get_annotation_from(frame)
+
+        cv2.imshow('Hand landmarks detection', annotation)
+    else:
+        print("! No frame")
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # After the loop release the cap object
-vid.release()
+cap.release()
+
 # Destroy all the windows
 cv2.destroyAllWindows()
