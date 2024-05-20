758c54f1fb6abba88b873576f205f9e476ba70f5
chuansate <lowsimchuan@gmail.com>
2024-05-20 22:31:14 +0800

    Added page for setting the set count and rep count
    Might consider making the buttons unclickable

--- workout_plan_page.py
+++ workout_plan_page.py
@@ -16,9 +16,55 @@ def go_back(uname, window):
     home_page(uname)
 
 
+def validate_set_rep_counts(workout_plan_window, setting_set_rep_window, set_value, rep_value, uname):
+    from counting_biceps_curl import render_counting_biceps_curl_UI
+    invalid_inputs = False
+    try:
+        rep_count = int(rep_value)
+        set_count = int(set_value)
+        if 1 <= rep_count <= 20 and 1 <= set_count <= 6:
+            invalid_inputs = False
+        else:
+            invalid_inputs = True
+    except ValueError:
+        invalid_inputs = True
+    if invalid_inputs:
+        msg = messagebox.showinfo("Warning", "Invalid inputs, try again.")
+    else:
+        workout_plan_window.destroy()
+        render_counting_biceps_curl_UI(uname, setting_set_rep_window)
+
+
+def setting_set_rep_counts_page(uname, workout_plan_window):
+    root = Tk()
+    root.title("Workout Set & Rep Counter")
+    WINDOW_WIDTH = 250
+    WINDOW_HEIGHT = 150
+    root.geometry(str(WINDOW_WIDTH) + "x" + str(WINDOW_HEIGHT))
+    root.resizable(False, False)
+    # Labels
+    set_label = Label(root, text="Sets (Max 6):")
+    set_label.grid(row=0, column=0, pady=5)
+
+    rep_label = Label(root, text="Reps (Max 20):")
+    rep_label.grid(row=1, column=0, pady=5)
+
+    # Entries with validation
+    set_entry = Entry(root)
+    set_entry.grid(row=0, column=1, pady=5)
+
+    rep_entry = Entry(root)
+    rep_entry.grid(row=1, column=1, pady=5)
+
+    # Button
+    start_button = Button(root, text="Start Workout", command=lambda: validate_set_rep_counts(workout_plan_window, root, set_entry.get(), rep_entry.get(), uname))
+    start_button.grid(row=2, column=0, pady=5, columnspan=2)
+
+    root.mainloop()
+
+
 def workout_plan_page(uname, window):
     from counting_biceps_curl import render_counting_biceps_curl_UI
-    from counting_push_up import render_counting_push_up_UI
     if window is not None:
         # window could be None happen when the user is from push-up/biceps curl to fitness_games_page
         window.destroy()
@@ -48,7 +94,7 @@ def workout_plan_page(uname, window):
     push_up_img = ImageTk.PhotoImage(push_up_img)
     push_up_button = tk.Button(window, text="Push-up", font=button_font, bg=button_bg, fg=button_fg,
                                   activebackground=button_active_bg, image=push_up_img, compound=RIGHT,
-                                  command=lambda: render_counting_push_up_UI(uname, window))
+                                  command=lambda: setting_set_rep_counts_page(uname, window))
     push_up_button.pack(pady=10, ipadx=20, ipady=10)
 
     biceps_curl_img = Image.open(PATH_TO_BICEPS_CURL_ICON)
