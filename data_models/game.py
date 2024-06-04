fa39b4f726c4dfe81c7735ba0de00664acf1aff0
chuansate <lowsimchuan@gmail.com>
2024-06-04 16:32:05 +0800

    Loaded the leaderboard data from the database

--- data_models/game.py
+++ data_models/game.py
@@ -1,4 +1,4 @@
-from .database_constants import HOST, DATABASE_NAME, GAMES_COLLECTION_NAME
+from .database_constants import HOST, DATABASE_NAME, GAMES_COLLECTION_NAME, USERS_COLLECTION_NAME
 import pymongo
 
 
@@ -16,6 +16,36 @@ class Game:
         })
         client.close()
 
+    def get_all_games_ids_names(self):
+        client = pymongo.MongoClient(HOST)
+        db = client[DATABASE_NAME]
+        games_col = db[GAMES_COLLECTION_NAME]
+        rows = []
+        for rec in games_col.find({}, {"game_name": 1}):
+            rows.append(rec)
+        client.close()
+        return rows
+
+    def get_best_records_by_each_user(self):
+        """
+
+        :return: best_records_by_each_user: keys are game IDs in string, values are lists of tuples (player_name, score)
+        """
+        client = pymongo.MongoClient(HOST)
+        db = client[DATABASE_NAME]
+        users_col = db[USERS_COLLECTION_NAME]
+        games_col = db[GAMES_COLLECTION_NAME]
+        best_records_by_each_user = {}
+        games_ids_strs = []
+        for rec in games_col.find({}, {"_id": 1, "game_name": 0, "description": 0}):
+            best_records_by_each_user[str(rec["_id"])] = []
+            games_ids_strs.append(str(rec["_id"]))
+        for row in users_col.find():
+            for id_str in games_ids_strs:
+                score = row["best_records"][id_str]
+                best_records_by_each_user[id_str].append((row["uname"], score))
+        client.close()
+        return best_records_by_each_user
 
 
 
--- game_leaderboard.py
+++ game_leaderboard.py
@@ -3,19 +3,28 @@ from tkinter import *
 from tkinter import ttk, messagebox
 from PIL import Image, ImageTk
 from paths_to_images import PATH_TO_SCORE_IMG
-# Sample leaderboard data
-leaderboard_data = {
-    "Kick & Catch": [
-        ("Alice", 1000),
-        ("Bob", 850),
-        ("Charlie", 700),
-    ],
-    "Yoga Imitation": [
-        ("David", 98),
-        ("Eve", 95),
-        ("Frank", 90),
-    ],
-}
+from data_models import Game
+from data_models import User
+
+# leaderboard_data = {
+#     "Kick & Catch": [
+#         ("Alice", 1000),
+#         ("Bob", 850),
+#         ("Charlie", 700),
+#     ],
+#     "Yoga Imitation": [
+#         ("David", 98),
+#         ("Eve", 95),
+#         ("Frank", 90),
+#     ],
+# }
+
+# the keys are game names, and the values are lists of tuples (player_name, score)
+leaderboard_data = {}
+
+
+def return_score_only(tup):
+    return tup[1]
 
 
 def go_back(uname, window):
@@ -24,13 +33,24 @@ def go_back(uname, window):
     fitness_games_page(uname, None)
 
 
+def load_leaderboard_data(top_number):
+    g = Game()
+    games_ids_names_list = g.get_all_games_ids_names()
+    best_records_by_each_user = g.get_best_records_by_each_user()
+    for row in games_ids_names_list:
+        best_records_by_each_user[str(row["_id"])].sort(key=return_score_only, reverse=True)
+
+    for row in games_ids_names_list:
+        leaderboard_data[row["game_name"]] = best_records_by_each_user[str(row["_id"])][:top_number]
+
+
 # Function to update leaderboard display
 def update_leaderboard(frame, game, rec_width):
     tk.Label(frame, text=game + " Leaderboard").pack(pady=10)
     # DK WHY ISNT IT SHOWING THE TROPHY ICON??
     score_img = Image.open(PATH_TO_SCORE_IMG)
     score_img = ImageTk.PhotoImage(score_img)
-    for i, (player, score) in enumerate(leaderboard_data[game][:3]):
+    for i, (player, score) in enumerate(leaderboard_data[game]):
         rec = tk.Frame(frame, bg="gray", height=35, width=rec_width)
         rec.pack(pady=5, side=TOP)
         index_label = tk.Label(rec, text=f"{i+1}. ", fg="white", bg="gray", font=("Arial", 14))
@@ -48,10 +68,14 @@ def leaderboard_page(uname, window):
     if window is not None:
         # window could be None happen when the user is from kick_and_catch_game/yoga_poses_imitation_game to fitness_games_page
         window.destroy()
+
+    # displaying top 3 players only in each game
+    load_leaderboard_data(top_number=3)
     WINDOW_WIDTH = 700
     WINDOW_HEIGHT = 400
     window = tk.Tk()
     window.geometry(str(WINDOW_WIDTH) + "x" + str(WINDOW_HEIGHT))
+    window.resizable(False, False)
     window.title("Leaderboard page")
     # Create notebook with tabs for each game
     notebook = ttk.Notebook(window)
@@ -61,9 +85,9 @@ def leaderboard_page(uname, window):
     kick_and_catch_frame = ttk.Frame(notebook)
     yoga_imitation_frame = ttk.Frame(notebook)
     # Update initial leaderboard display
-    update_leaderboard(kick_and_catch_frame, "Kick & Catch", WINDOW_WIDTH-100)
+    update_leaderboard(kick_and_catch_frame, "Kick-And-Catch", WINDOW_WIDTH-100)
     update_leaderboard(yoga_imitation_frame, "Yoga Imitation", WINDOW_WIDTH-100)
-    notebook.add(kick_and_catch_frame, text="Kick & Catch")
+    notebook.add(kick_and_catch_frame, text="Kick-And-Catch")
     notebook.add(yoga_imitation_frame, text="Yoga Imitation")
 
     logout_button = tk.Button(window, text="Go back", command=lambda: go_back(uname, window))
